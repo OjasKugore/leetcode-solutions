86156e984cf3aa73edc124594fcec7f1ad607a4a
OjasKugore <ojasmkugore.cs25@rvce.edu.in>
2026-06-28 08:07:29 +0530

    Update solution.py

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {} #value:index
+        seen = {} 
 
         for i in range(len(nums)):
             complement = target - nums[i]
