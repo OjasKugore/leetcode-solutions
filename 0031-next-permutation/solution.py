511b554dfcd0aadff619eec0387369845c99d7a1
OjasKugore <ojasmkugore.cs25@rvce.edu.in>
2026-07-03 04:36:08 +0000

    Sync LeetCode submission Runtime - 4 ms (1.96%), Memory - 19.2 MB (64.55%)

--- 0031-next-permutation/solution.py
+++ 0031-next-permutation/solution.py
@@ -3,22 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #find the breakpoint
-        pivot = -1
-        n = len(nums)
-        for i in range (n-2, -1, -1):
-            if (nums[i] < nums[i+1]):
-                pivot = i
-                break
-
-        if pivot == -1:
-            nums.reverse()
-            return
-        #swap breakpoint with smallest number on its right
-        for i in range (n-1, pivot, -1):
-            if (nums[pivot] < nums[i]):
-                nums[pivot], nums[i] = nums[i], nums[pivot] 
-                break
-
-        #reverse from pivot to end
-        nums[pivot + 1:]  = reversed(nums[pivot+1:])
+        if (nums == sorted(nums, reverse = True)):
+            nums.sort()
+        else:
+            n = len(nums)
+            for i  in range (n-2, -1, -1):
+                if nums[i] < nums[i+1]:
+                    pivot = i
+                    break
+            for i in range (n-1, pivot, -1):
+                if nums[i] > nums[pivot]:
+                    nums[i], nums[pivot] = nums[pivot], nums[i]
+                    break
+            nums[pivot+1:] = nums[pivot+1:][::-1]
