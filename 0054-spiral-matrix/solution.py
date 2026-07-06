d8bb2c995371057ce904698b988078320b5de824
OjasKugore <ojasmkugore.cs25@rvce.edu.in>
2026-07-06 18:06:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (33.52%)

--- 0054-spiral-matrix/solution.py
+++ 0054-spiral-matrix/solution.py
@@ -1,27 +1,26 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        m = len(matrix)
-        n = len(matrix[0])
-        top = 0
-        bottom = m
         left = 0
-        right = n
+        right = len(matrix[0]) -1 
+        top = 0
+        bottom  = len(matrix) - 1
         ans = []
 
-        while top<bottom and left<right:
-            for j in range (left, right):
-                ans.append(matrix[top][j])
-            top+=1
-            for i in range (top, bottom):
-                ans.append(matrix[i][right-1])
-            right-=1
-            if left>=right or top>=bottom:
-                break
-            for j in range (right-1, left-1, -1):
-                ans.append(matrix[bottom-1][j])
-            bottom -=1
-            for i in range (bottom-1, top-1, -1):
-                ans.append(matrix[i][left])
-            left+=1
-            
+        while left<=right and top <=bottom:
+            for col in range (left, right +1 ):
+                ans.append(matrix[top][col])
+            top +=1
+
+            for row in range (top, bottom + 1):
+                ans.append(matrix[row][right])
+            right -=1
+
+            if top<=bottom:
+                for col in range (right, left -1, -1):
+                    ans.append(matrix[bottom][col])
+                bottom -=1 
+            if left<=right:
+                for row in range (bottom, top -1, -1):
+                    ans.append(matrix[row][left])
+                left +=1 
         return ans
