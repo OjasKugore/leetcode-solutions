9e200de01727a4925c8147f3492afa0824bfc0e3
OjasKugore <ojasmkugore.cs25@rvce.edu.in>
2026-09-19 06:23:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (27.40%)

--- 0031-next-permutation/solution.py
+++ 0031-next-permutation/solution.py
@@ -1,18 +1,22 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if (nums == sorted(nums, reverse = True)):
-            nums.sort()
-        else:
-            n = len(nums)
-            for i  in range (n-2, -1, -1):
-                if nums[i] < nums[i+1]:
-                    pivot = i
-                    break
-            for i in range (n-1, pivot, -1):
-                if nums[i] > nums[pivot]:
+        pivot = -1
+        n = len(nums)
+        for i in range(n-2, -1, -1):
+            if (nums[i] < nums[i+1]):
+                pivot = i
+                break
+        if pivot != -1:
+            for i in range (n-1, -1, -1):
+                if (nums[i] > nums[pivot]):
                     nums[i], nums[pivot] = nums[pivot], nums[i]
                     break
-            nums[pivot+1:] = nums[pivot+1:][::-1]
+        left = pivot + 1
+        right = n-1
+        while (left < right):
+            nums[left] , nums[right] = nums[right] , nums[left]
+            left+=1
+            right-=1
